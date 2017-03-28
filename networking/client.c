a9eb9f2d5eb945c0d83ce02657bf86db128696be
Ellen Nie <xnie8@siebl-0222-18.ews.illinois.edu>
2017-03-28 00:42:07 -0500

    changed to UDP protocol

--- networking/client.c
+++ networking/client.c
@@ -1,13 +1,16 @@
-#include <sys/socket.h>
+
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
+
+#include <sys/socket.h> // For socket(), connect(), send(), and recv()
+#include <netinet/in.h>  // For sockaddr_in
+#include <arpa/inet.h> // For inet_addr()
+#include <netdb.h>  //For gethostbyname()
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h> 
+#include <unistd.h> //for close()
+#include <errno.h>  //for errno
+#include <string.h>  //for memset
+#include <sys/types.h>  //data types  
 
 /*struct sockaddr_in {
     short            sin_family;   
@@ -19,15 +22,107 @@
 /*struct in_addr {
     unsigned long s_addr;  // load with inet_aton()
 };*/
+typedef struct servent servent;
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+typedef struct hostent hostent;
 
+int broadcastPermission = 1;
+int reuse = 1;
+
+/*unsigned short resolveService(char * service, char* protocol){
+    servent * serv;
+    if ((serv = getservbyname(service.c_str(), protocol.c_str())) == NULL) {
+        return atoi(service.c_str());
+    }
+    else
+        return ntohs(serv->s_port);
+}*/
+static void fillAddr(const char * address, unsigned short port, sockaddr_in * addr) {
+    
+    //fprintf(stderr, "%d: %p\n", __LINE__, addr);
+    addr->sin_family = AF_INET;
+   // fprintf(stderr, "%d\n", __LINE__);
+    hostent *host; 
+    if((host = gethostbyname(address)) == NULL){  // ??
+        herror("fillAddr::gethostbyname()");
+        exit(2);
+    }
+    //fprintf(stderr, "%d\n", __LINE__);
+    addr->sin_addr.s_addr = *((unsigned long *) host->h_addr_list[0]);
+    //fprintf(stderr, "%d\n", __LINE__);
+    addr->sin_port = htons(port);
+}
+
+int createSocket(int type, int protocol){
+     int socket_desc = socket(PF_INET, type, protocol);
+     if(socket_desc <0){
+        perror("socket()");
+        exit(1);
+    }
+    return socket_desc;
+}
+
+void cleanup(int socket_desc){
+    close(socket_desc);
+}
+
+
+void sendTo(int socket_desc, const void *buffer, int bufferLen, const char *foreignAddress, unsigned short foreignPort) {
+    sockaddr_in destAddr;
+    memset(&destAddr, 0, sizeof(destAddr));
+    fprintf(stderr, "%d: %d\n", __LINE__, bufferLen);
+    fillAddr(foreignAddress, foreignPort, &destAddr);
+
+  // Write out the whole buffer as a single message.
+    if (sendto(socket_desc, buffer, bufferLen, 0,
+             (sockaddr *) &destAddr, sizeof(destAddr)) != bufferLen) {
+        perror("sendTo::sendto()");
+        exit(1);
+    }
+}
+
+
+
+
+void usage(char * command){
+    fprintf(stderr, "Usage: %s <Server> <Server Port>\n", command);
+}
 
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
-    {
-        fprintf(stderr, "Too free arguments");
-        return 1;
-    } 
+    if (argc!=3) { // Test for correct number of arguments
+        usage(argv[0]);
+        exit(1);
+    }
+
+    char * server_address = argv[1];
+    unsigned short server_port = atoi(argv[2]);
+
+    int socket_desc = createSocket(SOCK_DGRAM, IPPROTO_UDP); 
+
+    //setsockopt(socket_desc, SOL_SOCKET, SO_BROADCAST, &broadcastPermission, sizeof(broadcastPermission));
+    //setsockopt(socket_desc, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse));
+    //setLocalPort(socket_desc, port);
+
+
+    char* sendString = argv[3];               // Third arg:  string to broadcast
+
+
+    while(1){
+        char* msg = "Hello Kugo | Miao miao miao miao miao";
+        sendTo(socket_desc, msg, strlen(msg), server_address, server_port);
+        sleep(3);
+        //break;
+    }
+
+    cleanup(socket_desc);
+
+    return 0;
+}
+
+
+/*
 
     int sockfd = 0, n = 0;
     struct sockaddr_in serv_addr; 
@@ -40,7 +135,7 @@ int main(int argc, char *argv[])
         printf("\n Failed to create socket \n");
         return 1;
     } 
-    fprintf(stderr, "%d\n", __LINE__);
+    //fprintf(stderr, "%d\n", __LINE__);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
     memset(&(serv_addr.sin_zero), '0', 8); 
@@ -50,17 +145,16 @@ int main(int argc, char *argv[])
         printf("\nFailed to interpret IP address\n");
         return 1;
     } 
-    fprintf(stderr, "%d\n", __LINE__);
+
     if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
        printf("\n Failed to connect\n");
        return 1;
     } 
-	fprintf(stderr, "%d\n", __LINE__);
+
     while ( (n = recv(sockfd, recvBuff, sizeof(recvBuff)-1, 0)) > 0)
     {
         recvBuff[n] = 0;
-	printf("%d\n", __LINE__);
         if(fputs(recvBuff, stdout) == EOF)
         {
             printf("\n Fputs error\n");
@@ -73,4 +167,4 @@ int main(int argc, char *argv[])
     } 
 
     return 0;
-}
+}*/
