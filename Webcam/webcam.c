82c216c78b557cbbfbc523040c6c584701e84938
hwu63 <sharonwuhuiyun@outlook.com>
2017-03-27 23:35:19 -0500

    hahaha

--- Webcam/webcam.c
+++ Webcam/webcam.c
@@ -16,39 +16,41 @@ capture = cvCaptureFromCAM(0);//cvCaptureFromAVI("drop.avi");//
 if( !capture )
     printf( "Error when reading steam_avi");
 
-cvNamedWindow( "w", CV_WINDOW_AUTOSIZE);
-int num = 0;
-int x = 1;
-char s[20];
-
-/*int fps = 60;//(int)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
-int frameW= (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
-int frameH= (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
-  CvVideoWriter *writer=cvCreateVideoWriter("camera60.avi", CV_FOURCC('M','J','P','G'), fps,cvSize(frameW, frameH), 1);*/
-
 int param = CV_IMWRITE_JPEG_QUALITY;
-while(1)
-{
+
 	img = cvQueryFrame( capture );
 	
-	return cvEncodeImage(".jpg", img, &param );
+	CvMat* encoded = cvEncodeImage(".jpg", img, &param );
 	//cvShowImage("w",img);
 
 	char c = cvWaitKey(10);
-	if(c =='x') break;
-	if(c =='f'){
-		sprintf(s, "frame%d.jpg", num);
-		cvSaveImage(s, img, &x);
-	}
-	//img = cvLoadImage(s, 1);
-	//cvWriteFrame(writer,img);
-}
+	if(c =='x') return NULL;
+	
 cvReleaseImage(&img);
-cvDestroyWindow("w");
-//cvReleaseVideoWriter(&writer);
 
+return encoded;
 }
 
+
+void decode(CvMat * frame){
+IplImage* img = cvDecodeImage(frame, CV_LOAD_IMAGE_COLOR);
+
+cvNamedWindow( "w", CV_WINDOW_AUTOSIZE);
+
+int fps = 60;//(int)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+int frameW= (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+int frameH= (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+  CvVideoWriter *writer=cvCreateVideoWriter("camera60.avi", CV_FOURCC('M','J','P','G'),
+                       fps,cvSize(frameW, frameH), 1);
+cvShowImage("w",img);
+	char c = cvWaitKey(10);
+	if(c =='f'){
+		cvWriteFrame(writer,img);
+	}
+	
+	cvReleaseImage(&img);
+	cvDestroyWindow("w");
+}
 ////////////////////////////////////////////////////////////////////////////////
 /*int main(int argc, char** argv)
 {
